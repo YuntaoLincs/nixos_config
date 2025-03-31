7b1d785410ad23645899a8631223358a50236390
林云涛 <linyuntao@hayashis-MacBook-Pro.local>
2025-03-31 11:28:58 +0800

    Nix darwin

---
+++ home.nix
@@ -0,0 +1,63 @@
+# home.nix
+{ pkgs, config, ... }: {
+  # Home Manager options go here
+  home.username = "linyuntao"; # Set the user name (change as needed)
+  home.homeDirectory = "/Users/linyuntao"; # Set the home directory
+  home.stateVersion = "25.05";
+
+  # Example: Add some packages
+  home.packages = with pkgs; [
+    vscode # Install VSCode package here
+    yazi
+    helix
+    git
+  ];
+
+  home.file = { ".vimrc".source = ./dot_file/vim_configuration; };
+
+  xdg.configFile."helix" = {
+    source = ./dot_file/helix;
+    recursive = true;
+  };
+
+  programs.git = {
+    enable = true;
+    igores = [ ];
+    userEmail = "lin123456steve@outlook.com";
+    userName = "YuntaoLincs";
+  };
+
+  # Enable VSCode and configure extensions via Home Manager
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [ vscodevim.vim bbenoist.nix ];
+  };
+
+  programs.yazi = { enable = true; };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "autumn_night_transparent";
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+    };
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }];
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = { };
+      };
+    };
+  };
+
+  programs.home-manager.enable = true;
+
+}
